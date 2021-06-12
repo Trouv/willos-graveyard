952280d191dc636990230a0f78ea7f66e4bef0aa
TLovell <trevorlovelldesign@gmail.com>
2021-06-11 23:28:07 -0600

    Pushing is in, could be written better

--- src/gameplay/systems.rs
+++ src/gameplay/systems.rs
@@ -1,4 +1,7 @@
-use crate::gameplay::{components::*, xy_translation, Direction, MovementEvent};
+use crate::{
+    gameplay::{components::*, xy_translation, Direction, MovementEvent},
+    LevelSize,
+};
 use bevy::prelude::*;
 use bevy_easings::*;
 
@@ -49,12 +52,55 @@ pub fn simple_movement(
     }
 }
 
+fn push_tile_recurse(
+    collision_map: Vec<Vec<Option<(Entity, RigidBody)>>>,
+    pusher_coords: IVec2,
+    direction: Direction,
+) -> Vec<Entity> {
+    let pusher = collision_map[pusher_coords.y as usize][pusher_coords.x as usize]
+        .expect("pusher should exist")
+        .0;
+    let destination = pusher_coords + direction.into();
+    match collision_map[destination.y as usize][destination.x as usize] {
+        None => vec![pusher],
+        Some((_, RigidBody::Static)) => Vec::new(),
+        Some((_, RigidBody::Dynamic)) => {
+            let mut pushed_entities = push_tile_recurse(collision_map, destination, direction);
+            if pushed_entities.is_empty() {
+                Vec::new()
+            } else {
+                pushed_entities.push(pusher);
+                pushed_entities
+            }
+        }
+    }
+}
+
 pub fn perform_tile_movement(
-    mut tile_query: Query<&mut Tile, With<Pushable>>,
+    mut tile_query: Query<(Entity, &mut Tile, &RigidBody)>,
     mut reader: EventReader<MovementEvent>,
+    level_size: Res<LevelSize>,
 ) {
     for movement_event in reader.iter() {
-        let mut player_tile = tile_query.get_mut(movement_event.player).unwrap();
-        player_tile.coords += movement_event.direction.into();
+        let mut collision_map: Vec<Vec<Option<(Entity, RigidBody)>>> =
+            vec![vec![None; level_size.size.x as usize]; level_size.size.y as usize];
+        for (entity, tile, rigid_body) in tile_query.iter_mut() {
+            collision_map[tile.coords.y as usize][tile.coords.x as usize] =
+                Some((entity, *rigid_body));
+        }
+
+        let player_tile = tile_query
+            .get_component::<Tile>(movement_event.player)
+            .unwrap();
+
+        let pushed_entities =
+            push_tile_recurse(collision_map, player_tile.coords, movement_event.direction);
+
+        for entity in pushed_entities {
+            tile_query
+                .get_component_mut::<Tile>(entity)
+                .expect("Pushed should have Tile component")
+                .coords += movement_event.direction.into();
+        }
     }
 }
