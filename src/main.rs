e7e0efb1a558f72e5d30b4ffd964153ff49d4197
Trevor Lovell <trevorlovelldesign@gmail.com>
2022-09-10 20:41:52 -0600

    feat: make level selectable by identifier from command line if argument isn't an integer

--- src/main.rs
+++ src/main.rs
@@ -41,10 +41,14 @@ pub enum GameState {
 }
 
 fn main() {
-    let mut level_num = 1;
-    if std::env::args().count() > 1 {
-        level_num = std::env::args().last().unwrap().parse::<usize>().unwrap();
-    }
+    let level_selection = if std::env::args().count() > 1 {
+        match std::env::args().last().unwrap().parse::<usize>() {
+            Ok(num) => LevelSelection::Index(num - 1),
+            _ => LevelSelection::Identifier(std::env::args().last().unwrap()),
+        }
+    } else {
+        LevelSelection::Index(0)
+    };
 
     let mut app = App::new();
 
@@ -73,7 +77,7 @@ fn main() {
             ..default()
         })
         .insert_resource(Msaa { samples: 1 })
-        .insert_resource(LevelSelection::Index(level_num - 1))
+        .insert_resource(level_selection)
         .insert_resource(resources::GoalGhostSettings::NORMAL)
         .insert_resource(resources::RewindSettings::NORMAL)
         .insert_resource(resources::PlayZonePortion(0.75))
