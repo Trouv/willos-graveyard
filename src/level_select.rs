fa118a996bce19fb38b2c25b4a5a145c47b84a0e
Trevor Lovell <trevorlovelldesign@gmail.com>
2022-11-03 00:34:52 -0600

    feat: add remainder of level select logic, fix styling

--- src/level_select.rs
+++ src/level_select.rs
@@ -1,9 +1,11 @@
 use crate::{
+    event_scheduler::{EventScheduler, EventSchedulerPlugin},
+    gameplay::{components::UiRoot, systems::schedule_level_card, LevelCardEvent},
     nine_slice::{
         generate_nineslice_image, texture_atlas_from_nine_slice, NineSliceIndex, NineSliceSize,
     },
     ui::{actions::UiAction, text_button},
-    AssetHolder,
+    AssetHolder, GameState,
 };
 use bevy::prelude::*;
 use bevy_easings::*;
@@ -14,17 +16,52 @@ use std::time::Duration;
 pub struct LevelSelectPlugin;
 
 impl Plugin for LevelSelectPlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_enter_system(GameState::LevelSelect, spawn_level_select_card)
+            .add_plugin(EventSchedulerPlugin::<LevelSelectCardEvent>::new())
+            .add_system(pause.run_in_state(GameState::Gameplay))
+            .add_system(unpause.run_in_state(GameState::LevelSelect))
+            .add_system(
+                select_level
+                    .run_in_state(GameState::LevelSelect)
+                    .run_on_event::<UiAction>(),
+            )
+            .add_exit_system(GameState::LevelSelect, drop_level_card)
+            .add_system(despawn_level_card.run_on_event::<LevelSelectCardEvent>());
+    }
 }
 
 #[derive(Copy, Clone, Debug, Default, Eq, PartialEq, Component)]
-pub struct LevelSelect;
+pub struct LevelSelectCard;
+
+pub enum LevelSelectCardEvent {
+    Spawned(Entity),
+    Falling(Entity),
+    Offscreen(Entity),
+}
+
+fn level_select_card_style(position: UiRect<Val>) -> Style {
+    Style {
+        justify_content: JustifyContent::Center,
+        align_items: AlignItems::Center,
+        flex_direction: FlexDirection::ColumnReverse,
+        position_type: PositionType::Absolute,
+        size: Size {
+            width: Val::Percent(100.),
+            height: Val::Percent(100.),
+        },
+        position,
+        ..default()
+    }
+}
 
 fn spawn_level_select_card(
     mut commands: Commands,
     asset_holder: Res<AssetHolder>,
     mut images: ResMut<Assets<Image>>,
     ldtk_assets: Res<Assets<LdtkAsset>>,
+    mut event_writer: EventWriter<LevelSelectCardEvent>,
+    ui_root: Query<Entity, With<UiRoot>>,
 ) {
     // TODO: refactor this to avoid repeated code with spawn_level_card
     let level_card_atlas = texture_atlas_from_nine_slice(
@@ -45,51 +82,31 @@ fn spawn_level_select_card(
         &mut images,
     )
     .unwrap();
-    commands
+    let level_select_entity = commands
         .spawn_bundle(ImageBundle {
             image: UiImage(level_card_texture),
             //visibility: Visibility { is_visible: false },
             ..default()
         })
         .insert(
-            Style {
-                justify_content: JustifyContent::Center,
-                flex_direction: FlexDirection::ColumnReverse,
-                position_type: PositionType::Absolute,
-                size: Size {
-                    width: Val::Percent(100.),
-                    height: Val::Percent(100.),
-                },
-                position: UiRect {
-                    top: Val::Percent(100.),
+            level_select_card_style(UiRect {
+                top: Val::Percent(100.),
+                left: Val::Percent(0.),
+                ..default()
+            })
+            .ease_to(
+                level_select_card_style(UiRect {
+                    top: Val::Percent(0.),
                     left: Val::Percent(0.),
                     ..default()
-                },
-                ..default()
-            }
-            .ease_to(
-                Style {
-                    justify_content: JustifyContent::Center,
-                    flex_direction: FlexDirection::ColumnReverse,
-                    position_type: PositionType::Absolute,
-                    size: Size {
-                        width: Val::Percent(100.),
-                        height: Val::Percent(100.),
-                    },
-                    position: UiRect {
-                        top: Val::Percent(0.),
-                        left: Val::Percent(0.),
-                        ..Default::default()
-                    },
-                    ..Default::default()
-                },
+                }),
                 EaseFunction::QuadraticOut,
                 EasingType::Once {
                     duration: Duration::from_secs(1),
                 },
             ),
         )
-        .insert(LevelSelect)
+        .insert(LevelSelectCard)
         .with_children(|parent| {
             // spawn title
             parent.spawn_bundle(TextBundle {
@@ -109,8 +126,8 @@ fn spawn_level_select_card(
                     margin: UiRect {
                         top: Val::Px(10.),
                         bottom: Val::Px(10.),
-                        left: Val::Px(10.),
-                        right: Val::Px(10.),
+                        left: Val::Percent(10.),
+                        right: Val::Percent(10.),
                     },
                     ..default()
                 },
@@ -122,25 +139,120 @@ fn spawn_level_select_card(
                 .spawn_bundle(NodeBundle {
                     color: UiColor(Color::NONE),
                     style: Style {
-                        flex_wrap: FlexWrap::Wrap,
-                        justify_content: JustifyContent::Center,
+                        flex_wrap: FlexWrap::WrapReverse,
+                        justify_content: JustifyContent::SpaceAround,
                         margin: UiRect {
                             top: Val::Px(10.),
                             bottom: Val::Px(10.),
-                            left: Val::Px(10.),
-                            right: Val::Px(10.),
+                            left: Val::Percent(10.),
+                            right: Val::Percent(10.),
+                        },
+                        size: Size {
+                            height: Val::Percent(60.),
+                            width: Val::Percent(80.),
                         },
                         ..default()
                     },
                     ..default()
                 })
                 .with_children(|parent| {
+                    // spawn a button for every level
                     if let Some(ldtk) = ldtk_assets.get(&asset_holder.ldtk) {
                         for (i, _) in ldtk.iter_levels().enumerate() {
-                            text_button::spawn(parent, format!("#{}", i + 1), &asset_holder)
-                                .insert(UiAction::GoToLevel(LevelSelection::Index(i)));
+                            text_button::spawn(
+                                parent,
+                                format!("#{}", i + 1),
+                                &asset_holder,
+                                Val::Percent(2.),
+                            )
+                            .insert(UiAction::GoToLevel(LevelSelection::Index(i)));
                         }
                     }
                 });
-        });
+        })
+        .id();
+
+    commands
+        .entity(ui_root.single())
+        .add_child(level_select_entity);
+
+    event_writer.send(LevelSelectCardEvent::Spawned(level_select_entity));
+}
+
+fn pause(mut commands: Commands, input: Res<Input<KeyCode>>) {
+    if input.just_pressed(KeyCode::Escape) || input.just_pressed(KeyCode::P) {
+        commands.insert_resource(NextState(GameState::LevelSelect));
+    }
+}
+
+fn unpause(mut commands: Commands, input: Res<Input<KeyCode>>) {
+    if input.just_pressed(KeyCode::Escape) || input.just_pressed(KeyCode::P) {
+        commands.insert_resource(NextState(GameState::Gameplay));
+    }
+}
+
+fn select_level(
+    mut commands: Commands,
+    mut ui_actions: EventReader<UiAction>,
+    mut level_card_events: ResMut<EventScheduler<LevelCardEvent>>,
+) {
+    for action in ui_actions.iter() {
+        match action {
+            UiAction::GoToLevel(level_selection) => {
+                commands.insert_resource(NextState(GameState::Gameplay));
+                schedule_level_card(&mut level_card_events, level_selection.clone());
+                commands.insert_resource(NextState(GameState::LevelTransition));
+            }
+            _ => (),
+        }
+    }
+}
+
+fn drop_level_card(
+    mut commands: Commands,
+    level_select_card_query: Query<Entity, With<LevelSelectCard>>,
+    mut level_select_card_events: ResMut<EventScheduler<LevelSelectCardEvent>>,
+) {
+    for entity in level_select_card_query.iter() {
+        commands.entity(entity).insert(
+            level_select_card_style(UiRect {
+                top: Val::Percent(0.),
+                left: Val::Percent(0.),
+                ..default()
+            })
+            .ease_to(
+                level_select_card_style(UiRect {
+                    top: Val::Percent(100.),
+                    left: Val::Percent(0.),
+                    ..default()
+                }),
+                EaseFunction::QuadraticOut,
+                EasingType::Once {
+                    duration: Duration::from_secs(1),
+                },
+            ),
+        );
+        level_select_card_events.schedule(
+            LevelSelectCardEvent::Falling(entity),
+            Duration::from_millis(0),
+        );
+        level_select_card_events.schedule(
+            LevelSelectCardEvent::Offscreen(entity),
+            Duration::from_millis(1000),
+        );
+    }
+}
+
+fn despawn_level_card(
+    mut commands: Commands,
+    mut level_select_card_events: EventReader<LevelSelectCardEvent>,
+) {
+    for event in level_select_card_events.iter() {
+        match event {
+            LevelSelectCardEvent::Offscreen(entity) => {
+                commands.entity(*entity).despawn_recursive();
+            }
+            _ => (),
+        }
+    }
 }
