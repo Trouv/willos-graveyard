753a781d2c6123a20d2f326a80a5ad8be04fcea9
Trevor Lovell <trevorlovelldesign@gmail.com>
2022-11-10 18:21:21 -0700

    refactor: update game to use TransitionTo resource

--- src/level_transition.rs
+++ src/level_transition.rs
@@ -17,7 +17,8 @@ pub struct LevelTransitionPlugin;
 
 impl Plugin for LevelTransitionPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugin(EventSchedulerPlugin::<LevelCardEvent>::new())
+        app.init_resource::<TransitionTo>()
+            .add_plugin(EventSchedulerPlugin::<LevelCardEvent>::new())
             .add_system(
                 trigger_level_transition_state
                     .run_not_in_state(GameState::AssetLoading)
@@ -25,11 +26,23 @@ impl Plugin for LevelTransitionPlugin {
                     .run_if_resource_added::<TransitionTo>(),
             )
             .add_exit_system(GameState::LevelTransition, clean_up_transition_to_resource)
-            .add_startup_system(schedule_first_level_card)
+            .add_enter_system(GameState::LevelTransition, spawn_level_card)
+            // level_card_update should be performed during both gameplay and level transition
+            // states since it cleans up the level card after it's done falling during the gameplay
+            // state
+            .add_system(
+                level_card_update
+                    .run_in_state(GameState::LevelTransition)
+                    .run_on_event::<LevelCardEvent>(),
+            )
+            .add_system(
+                level_card_update
+                    .run_in_state(GameState::Gameplay)
+                    .run_on_event::<LevelCardEvent>(),
+            )
             .add_system_set(
                 ConditionSet::new()
                     .run_in_state(GameState::LevelTransition)
-                    .with_system(spawn_level_card)
                     .with_system(load_next_level)
                     .with_system(level_card_update)
                     .into(),
@@ -38,6 +51,7 @@ impl Plugin for LevelTransitionPlugin {
 }
 
 /// Resource that can be inserted to trigger a level transition.
+#[derive(Clone, Eq, PartialEq, Debug, Default, Deref, DerefMut, Component)]
 pub struct TransitionTo(pub LevelSelection);
 
 /// Component that marks the level card and stores its current state.
@@ -53,8 +67,7 @@ enum LevelCard {
 /// of the animation.
 #[derive(Clone, Eq, PartialEq, Debug)]
 pub enum LevelCardEvent {
-    Rise(LevelSelection),
-    Block(LevelSelection),
+    Block,
     Fall,
     Despawn,
 }
@@ -62,27 +75,10 @@ pub enum LevelCardEvent {
 /// Utility for causing a level card to perform its animation.
 ///
 /// Must be accompanied by a change to the [GameState::LevelTransition] state to work.
-pub fn schedule_level_card(
-    level_card_events: &mut EventScheduler<LevelCardEvent>,
-    level_selection: LevelSelection,
-    offset_millis: u64,
-) {
-    level_card_events.schedule(
-        LevelCardEvent::Rise(level_selection.clone()),
-        Duration::from_millis(offset_millis),
-    );
-    level_card_events.schedule(
-        LevelCardEvent::Block(level_selection),
-        Duration::from_millis(1500 + offset_millis),
-    );
-    level_card_events.schedule(
-        LevelCardEvent::Fall,
-        Duration::from_millis(3000 + offset_millis),
-    );
-    level_card_events.schedule(
-        LevelCardEvent::Despawn,
-        Duration::from_millis(4500 + offset_millis),
-    );
+fn schedule_level_card(level_card_events: &mut EventScheduler<LevelCardEvent>) {
+    level_card_events.schedule(LevelCardEvent::Block, Duration::from_millis(1500));
+    level_card_events.schedule(LevelCardEvent::Fall, Duration::from_millis(3000));
+    level_card_events.schedule(LevelCardEvent::Despawn, Duration::from_millis(4500));
 }
 
 fn trigger_level_transition_state(mut commands: Commands) {
@@ -93,28 +89,18 @@ fn clean_up_transition_to_resource(mut commands: Commands) {
     commands.remove_resource::<TransitionTo>()
 }
 
-// TODO: change the logic for level cards to rise on entering `LevelTransition`, with some stage to
-// add buffer to it if needed.
-//
-// Loading the first level bugs out sometimes because this is a startup system.
-fn schedule_first_level_card(
-    mut level_card_events: ResMut<EventScheduler<LevelCardEvent>>,
-    level_selection: Res<LevelSelection>,
-) {
-    schedule_level_card(&mut level_card_events, level_selection.clone(), 800);
-}
-
 fn load_next_level(
     mut commands: Commands,
     mut level_card_events: EventReader<LevelCardEvent>,
     mut level_selection: ResMut<LevelSelection>,
     mut first_card_skipped: Local<bool>,
+    transition_to: Res<TransitionTo>,
     asset_holder: Res<AssetHolder>,
 ) {
     for event in level_card_events.iter() {
-        if let LevelCardEvent::Block(new_level_selection) = event {
+        if let LevelCardEvent::Block = event {
             if *first_card_skipped {
-                *level_selection = new_level_selection.clone()
+                *level_selection = transition_to.0.clone()
             } else {
                 commands.spawn_bundle(LdtkWorldBundle {
                     ldtk_handle: asset_holder.ldtk.clone(),
@@ -130,154 +116,154 @@ fn load_next_level(
 
 fn spawn_level_card(
     mut commands: Commands,
-    mut reader: EventReader<LevelCardEvent>,
+    mut level_card_events: ResMut<EventScheduler<LevelCardEvent>>,
+    transition_to: Res<TransitionTo>,
     ldtk_assets: Res<Assets<LdtkAsset>>,
     assets: Res<AssetServer>,
     asset_holder: Res<AssetHolder>,
     mut images: ResMut<Assets<Image>>,
     ui_root_query: Query<Entity, With<UiRoot>>,
 ) {
-    for event in reader.iter() {
-        if let LevelCardEvent::Rise(level_selection) = event {
-            let mut title = "Thank you for playing!\n\nMade by Trevor Lovell and Gabe Machado\n\nWayfarer's Toy Box font by Chequered Ink".to_string();
-            let mut level_num = None;
+    dbg!(&transition_to);
+    let mut title = "Thank you for playing!\n\nMade by Trevor Lovell and Gabe Machado\n\nWayfarer's Toy Box font by Chequered Ink".to_string();
+    let mut level_num = None;
 
-            if let Some(ldtk_asset) = ldtk_assets.get(&asset_holder.ldtk) {
-                if let Some((level_index, level)) = ldtk_asset
-                    .iter_levels()
-                    .enumerate()
-                    .find(|(i, level)| level_selection.is_match(i, level))
+    if let Some(ldtk_asset) = ldtk_assets.get(&asset_holder.ldtk) {
+        if let Some((level_index, level)) = ldtk_asset
+            .iter_levels()
+            .enumerate()
+            .find(|(i, level)| transition_to.is_match(i, level))
+        {
+            if level_index < ldtk_asset.project.levels.len() {
+                level_num = Some(level_index + 1);
+
+                if let Some(FieldInstance {
+                    value: FieldValue::String(Some(level_title)),
+                    ..
+                }) = level
+                    .field_instances
+                    .iter()
+                    .find(|f| f.identifier == "Title")
                 {
-                    if level_index < ldtk_asset.project.levels.len() {
-                        level_num = Some(level_index + 1);
-
-                        if let Some(FieldInstance {
-                            value: FieldValue::String(Some(level_title)),
-                            ..
-                        }) = level
-                            .field_instances
-                            .iter()
-                            .find(|f| f.identifier == "Title")
-                        {
-                            title = level_title.clone();
-                        }
-                    }
+                    title = level_title.clone();
                 }
             }
+        }
+    }
 
-            let level_card_atlas = texture_atlas_from_nine_slice(
-                asset_holder.tarot_sheet.clone(),
-                Vec2::splat(64.),
-                16.,
-                16.,
-                16.,
-                16.,
-            );
-            let level_card_texture = generate_nineslice_image(
-                NineSliceSize {
-                    inner_width: 8,
-                    inner_height: 4,
+    let level_card_atlas = texture_atlas_from_nine_slice(
+        asset_holder.tarot_sheet.clone(),
+        Vec2::splat(64.),
+        16.,
+        16.,
+        16.,
+        16.,
+    );
+    let level_card_texture = generate_nineslice_image(
+        NineSliceSize {
+            inner_width: 8,
+            inner_height: 4,
+        },
+        NineSliceIndex::default(),
+        &level_card_atlas,
+        &mut images,
+    )
+    .unwrap();
+
+    let level_card_entity = commands
+        .spawn_bundle(ImageBundle {
+            image: UiImage(level_card_texture),
+            visibility: Visibility { is_visible: false },
+            ..Default::default()
+        })
+        .insert(
+            Style {
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                position_type: PositionType::Absolute,
+                flex_direction: FlexDirection::ColumnReverse,
+                size: Size {
+                    width: Val::Percent(100.),
+                    height: Val::Percent(100.),
                 },
-                NineSliceIndex::default(),
-                &level_card_atlas,
-                &mut images,
-            )
-            .unwrap();
-
-            let level_card_entity = commands
-                .spawn_bundle(ImageBundle {
-                    image: UiImage(level_card_texture),
+                position: UiRect {
+                    top: Val::Percent(100.),
+                    left: Val::Percent(0.),
+                    ..Default::default()
+                },
+                ..Default::default()
+            }
+            .ease_to(
+                Style {
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
+                    position_type: PositionType::Absolute,
+                    flex_direction: FlexDirection::ColumnReverse,
+                    size: Size {
+                        width: Val::Percent(100.),
+                        height: Val::Percent(100.),
+                    },
+                    position: UiRect {
+                        top: Val::Percent(0.),
+                        left: Val::Percent(0.),
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                },
+                EaseFunction::QuadraticOut,
+                EasingType::Once {
+                    duration: Duration::from_secs(1),
+                },
+            ),
+        )
+        .with_children(|parent| {
+            if let Some(level_num) = level_num {
+                parent
+                    .spawn_bundle(TextBundle {
+                        text: Text::from_section(
+                            format!("#{level_num}"),
+                            TextStyle {
+                                font: assets.load("fonts/WayfarersToyBoxRegular-gxxER.ttf"),
+                                color: Color::WHITE,
+                                ..default()
+                            },
+                        )
+                        .with_alignment(TextAlignment {
+                            vertical: VerticalAlign::Center,
+                            horizontal: HorizontalAlign::Center,
+                        }),
+                        visibility: Visibility { is_visible: false },
+                        ..Default::default()
+                    })
+                    .insert(FontScale::from(FontSize::Huge));
+            }
+            parent
+                .spawn_bundle(TextBundle {
+                    text: Text::from_section(
+                        title,
+                        TextStyle {
+                            font: assets.load("fonts/WayfarersToyBoxRegular-gxxER.ttf"),
+                            color: Color::WHITE,
+                            ..default()
+                        },
+                    ),
                     visibility: Visibility { is_visible: false },
                     ..Default::default()
                 })
-                .insert(
-                    Style {
-                        justify_content: JustifyContent::Center,
-                        align_items: AlignItems::Center,
-                        position_type: PositionType::Absolute,
-                        flex_direction: FlexDirection::ColumnReverse,
-                        size: Size {
-                            width: Val::Percent(100.),
-                            height: Val::Percent(100.),
-                        },
-                        position: UiRect {
-                            top: Val::Percent(100.),
-                            left: Val::Percent(0.),
-                            ..Default::default()
-                        },
-                        ..Default::default()
-                    }
-                    .ease_to(
-                        Style {
-                            justify_content: JustifyContent::Center,
-                            align_items: AlignItems::Center,
-                            position_type: PositionType::Absolute,
-                            flex_direction: FlexDirection::ColumnReverse,
-                            size: Size {
-                                width: Val::Percent(100.),
-                                height: Val::Percent(100.),
-                            },
-                            position: UiRect {
-                                top: Val::Percent(0.),
-                                left: Val::Percent(0.),
-                                ..Default::default()
-                            },
-                            ..Default::default()
-                        },
-                        EaseFunction::QuadraticOut,
-                        EasingType::Once {
-                            duration: Duration::from_secs(1),
-                        },
-                    ),
-                )
-                .with_children(|parent| {
-                    if let Some(level_num) = level_num {
-                        parent
-                            .spawn_bundle(TextBundle {
-                                text: Text::from_section(
-                                    format!("#{level_num}"),
-                                    TextStyle {
-                                        font: assets.load("fonts/WayfarersToyBoxRegular-gxxER.ttf"),
-                                        color: Color::WHITE,
-                                        ..default()
-                                    },
-                                )
-                                .with_alignment(TextAlignment {
-                                    vertical: VerticalAlign::Center,
-                                    horizontal: HorizontalAlign::Center,
-                                }),
-                                visibility: Visibility { is_visible: false },
-                                ..Default::default()
-                            })
-                            .insert(FontScale::from(FontSize::Huge));
-                    }
-                    parent
-                        .spawn_bundle(TextBundle {
-                            text: Text::from_section(
-                                title,
-                                TextStyle {
-                                    font: assets.load("fonts/WayfarersToyBoxRegular-gxxER.ttf"),
-                                    color: Color::WHITE,
-                                    ..default()
-                                },
-                            ),
-                            visibility: Visibility { is_visible: false },
-                            ..Default::default()
-                        })
-                        .insert(FontScale::from(FontSize::Medium));
-                })
-                .insert(if level_num.is_some() {
-                    LevelCard::Rising
-                } else {
-                    LevelCard::End
-                })
-                .id();
+                .insert(FontScale::from(FontSize::Medium));
+        })
+        .id();
 
-            commands
-                .entity(ui_root_query.single())
-                .add_child(level_card_entity);
-        }
+    if level_num.is_some() {
+        commands.entity(level_card_entity).insert(LevelCard::Rising);
+        schedule_level_card(&mut level_card_events);
+    } else {
+        commands.entity(level_card_entity).insert(LevelCard::End);
     }
+
+    commands
+        .entity(ui_root_query.single())
+        .add_child(level_card_entity);
 }
 
 fn level_card_update(
@@ -288,7 +274,7 @@ fn level_card_update(
     for event in level_card_events.iter() {
         for (entity, mut card, style) in card_query.iter_mut() {
             match event {
-                LevelCardEvent::Block(_) => {
+                LevelCardEvent::Block => {
                     *card = LevelCard::Holding;
                 }
                 LevelCardEvent::Fall => {
@@ -314,7 +300,6 @@ fn level_card_update(
                     // SELF DESTRUCT
                     commands.entity(entity).despawn_recursive();
                 }
-                _ => {}
             }
         }
     }
