836bf4fec77f28649b09811806f8690c24e3176f
Trevor Lovell <trevorlovelldesign@gmail.com>
2022-11-10 18:37:38 -0700

    fix: removed redundant level_card_update scheduling, simplify/reorganize LevelTransitionPlugin

--- src/level_transition.rs
+++ src/level_transition.rs
@@ -27,25 +27,21 @@ impl Plugin for LevelTransitionPlugin {
             )
             .add_exit_system(GameState::LevelTransition, clean_up_transition_to_resource)
             .add_enter_system(GameState::LevelTransition, spawn_level_card)
+            .add_system_set(
+                ConditionSet::new()
+                    .run_in_state(GameState::LevelTransition)
+                    .run_on_event::<LevelCardEvent>()
+                    .with_system(level_card_update)
+                    .with_system(load_next_level)
+                    .into(),
+            )
             // level_card_update should be performed during both gameplay and level transition
             // states since it cleans up the level card after it's done falling during the gameplay
             // state
             .add_system(
                 level_card_update
-                    .run_in_state(GameState::LevelTransition)
-                    .run_on_event::<LevelCardEvent>(),
-            )
-            .add_system(
-                level_card_update
                     .run_in_state(GameState::Gameplay)
                     .run_on_event::<LevelCardEvent>(),
-            )
-            .add_system_set(
-                ConditionSet::new()
-                    .run_in_state(GameState::LevelTransition)
-                    .with_system(load_next_level)
-                    .with_system(level_card_update)
-                    .into(),
             );
     }
 }
