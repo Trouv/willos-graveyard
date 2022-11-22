80ec3017f90b8411dffe0db80b339c1ff21ee722
Trevor Lovell <trevorlovelldesign@gmail.com>
2022-11-21 21:23:42 -0700

    fix: adjust spawn_control_display system to ensure there's only a single control_display entity

--- src/control_display.rs
+++ src/control_display.rs
@@ -13,7 +13,7 @@ pub struct ControlDisplayPlugin;
 
 impl Plugin for ControlDisplayPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(spawn_control_display.run_in_state(GameState::LevelTransition))
+        app.add_enter_system(GameState::LevelTransition, spawn_control_display)
             .add_system_to_stage(
                 CoreStage::PreUpdate,
                 update_control_display.run_in_state(GameState::Gameplay),
@@ -25,33 +25,41 @@ impl Plugin for ControlDisplayPlugin {
 #[derive(Copy, Clone, Eq, PartialEq, Debug, Default, Hash, Component)]
 struct ControlDisplay;
 
-fn spawn_control_display(mut commands: Commands, play_zone_portion: Res<PlayZonePortion>) {
-    let control_zone_ratio = 1. - **play_zone_portion;
+fn spawn_control_display(
+    mut commands: Commands,
+    play_zone_portion: Res<PlayZonePortion>,
+    mut already_spawned: Local<bool>,
+) {
+    if !*already_spawned {
+        let control_zone_ratio = 1. - **play_zone_portion;
 
-    commands
-        .spawn(NodeBundle {
-            background_color: BackgroundColor(Color::NONE),
-            style: Style {
-                flex_direction: FlexDirection::Column,
-                align_items: AlignItems::FlexStart,
-                justify_content: JustifyContent::Center,
-                align_content: AlignContent::Center,
-                position_type: PositionType::Absolute,
-                size: Size {
-                    width: Val::Percent(100. * control_zone_ratio),
-                    height: Val::Percent(100.),
-                },
-                position: UiRect {
-                    top: Val::Percent(0.),
-                    right: Val::Percent(0.),
+        commands
+            .spawn(NodeBundle {
+                background_color: BackgroundColor(Color::NONE),
+                style: Style {
+                    flex_direction: FlexDirection::Column,
+                    align_items: AlignItems::FlexStart,
+                    justify_content: JustifyContent::Center,
+                    align_content: AlignContent::Center,
+                    position_type: PositionType::Absolute,
+                    size: Size {
+                        width: Val::Percent(100. * control_zone_ratio),
+                        height: Val::Percent(100.),
+                    },
+                    position: UiRect {
+                        top: Val::Percent(0.),
+                        right: Val::Percent(0.),
+                        ..Default::default()
+                    },
                     ..Default::default()
                 },
+                z_index: ZIndex::Local(-1),
                 ..Default::default()
-            },
-            z_index: ZIndex::Local(-1),
-            ..Default::default()
-        })
-        .insert(ControlDisplay);
+            })
+            .insert(ControlDisplay);
+
+        *already_spawned = true;
+    }
 }
 
 fn update_control_display(
