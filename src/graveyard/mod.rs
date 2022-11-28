ab280d0c482673fd3ec780178b9441496708d927
Trevor Lovell <trevorlovelldesign@gmail.com>
2022-11-27 19:17:30 -0700

    refactor: move graveyard app builders into new GraveyardPlugin

--- src/graveyard/mod.rs
+++ src/graveyard/mod.rs
@@ -1,3 +1,5 @@
+use bevy::prelude::*;
+
 pub mod control_display;
 pub mod exorcism;
 pub mod goal;
@@ -6,3 +8,18 @@ pub mod movement_table;
 pub mod sokoban;
 pub mod willo;
 pub mod wind;
+
+pub struct GraveyardPlugin;
+
+impl Plugin for GraveyardPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugin(control_display::ControlDisplayPlugin)
+            .add_plugin(willo::WilloPlugin)
+            .add_plugin(sokoban::SokobanPlugin)
+            .add_plugin(movement_table::MovementTablePlugin)
+            .add_plugin(gravestone::GravestonePlugin)
+            .add_plugin(goal::GoalPlugin)
+            .add_plugin(exorcism::ExorcismPlugin)
+            .add_plugin(wind::WindPlugin);
+    }
+}
