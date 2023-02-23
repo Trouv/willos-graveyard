265a6282e7480547fd0784562de87e9aedb73257
Trevor Lovell <trevorlovelldesign@gmail.com>
2023-02-22 22:05:16 -0700

    feat: add some components and a skeleton for the main system

--- src/ui/icon_button.rs
+++ src/ui/icon_button.rs
@@ -1,9 +1,30 @@
 use bevy::prelude::*;
 
+use crate::ui_atlas_image::UiAtlasImage;
+
 struct IconButtonPlugin;
 
 impl Plugin for IconButtonPlugin {
     fn build(&self, app: &mut App) {
         todo!()
+    }
+}
+
+#[derive(Default, Debug, Component)]
+pub struct IconButton {
+    pub icon: UiAtlasImage,
+}
+
+struct ButtonRadial;
+
+fn spawn_icon_button_elements(
+    mut commands: Commands,
+    icon_buttons: Query<(Entity, &IconButton), Changed<IconButton>>,
+) {
+    for (entity, icon_button) in &icon_buttons {
+        commands.entity(entity).add_children(|parent| {
+            // Radial
+            todo!()
+        });
     }
 }
