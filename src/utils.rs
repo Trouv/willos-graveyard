deb08725bd8ae9bba77dccba15318fd99d0b6fa7
Trevor Lovell <trevorlovelldesign@gmail.com>
2023-03-01 22:15:27 -0700

    feat: add basic docs for utils module

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,7 @@
+//! Common utilities that may be useful in many contexts.
 use bevy::prelude::*;
 
+/// Simple `iyes_loopless` run condition that passes if the resource has changed.
 pub fn resource_changed<R: Resource>(resource: Res<R>) -> bool {
     resource.is_changed()
 }
