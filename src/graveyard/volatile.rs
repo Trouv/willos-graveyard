1f37dcbd21398b419e4e7f9debb65698826acf25
Trevor Lovell <trevorlovelldesign@gmail.com>
2023-11-04 12:46:16 -0600

    feat: add basic sublimation system (could be optimized with change detection maybe?)

--- src/graveyard/volatile.rs
+++ src/graveyard/volatile.rs
@@ -13,3 +13,25 @@ pub enum Volatile {
     /// The entity has collided with another volatile solid, and has been subliminated.
     Sublimated,
 }
+
+impl Volatile {
+    fn is_solid(&self) -> bool {
+        matches!(self, Volatile::Solid)
+    }
+
+    fn sublimate(&mut self) {
+        *self = Volatile::Sublimated;
+    }
+}
+
+fn sublimation(mut volatile_query: Query<(&GridCoords, &mut Volatile)>) {
+    let mut combinations = volatile_query.iter_combinations_mut::<2>();
+    while let Some([(grid_coords_a, mut volatile_a), (grid_coords_b, mut volatile_b)]) =
+        combinations.fetch_next()
+    {
+        if grid_coords_a == grid_coords_b && volatile_a.is_solid() && volatile_b.is_solid() {
+            volatile_a.sublimate();
+            volatile_b.sublimate();
+        }
+    }
+}
