7e5b40726b1dd5441390164ca5c4d9c8f3d39008
Trevor Lovell <trevorlovelldesign@gmail.com>
2023-11-04 12:45:59 -0600

    docs: add some docs for Volatile variants

--- src/graveyard/volatile.rs
+++ src/graveyard/volatile.rs
@@ -1,12 +1,15 @@
 use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
 
 /// Component defining the volatility of an entity and its volatile state.
 ///
-/// If two volatile solids share the same space, they both are sublimated.
+/// If two volatile solids share the same [`GridCoords`] space, they both are sublimated.
 /// What this means for a particular entity should be defined elsewhere.
 #[derive(Copy, Clone, Debug, Default, PartialEq, Eq, Component)]
 pub enum Volatile {
+    /// The entity is still a volatile solid.
     #[default]
     Solid,
+    /// The entity has collided with another volatile solid, and has been subliminated.
     Sublimated,
 }
