e544fce48209fe44af09e610cd81ab96147be223
Trevor Lovell <trevorlovelldesign@gmail.com>
2023-11-05 22:00:13 -0700

    feat: optimize sublimation system to only care about moved tiles in first layer of 2d iteration

--- src/graveyard/volatile.rs
+++ src/graveyard/volatile.rs
@@ -24,14 +24,37 @@ impl Volatile {
     }
 }
 
-fn sublimation(mut volatile_query: Query<(&GridCoords, &mut Volatile)>) {
-    let mut combinations = volatile_query.iter_combinations_mut::<2>();
-    while let Some([(grid_coords_a, mut volatile_a), (grid_coords_b, mut volatile_b)]) =
-        combinations.fetch_next()
-    {
-        if grid_coords_a == grid_coords_b && volatile_a.is_solid() && volatile_b.is_solid() {
-            volatile_a.sublimate();
-            volatile_b.sublimate();
+fn sublimation(
+    moved_volatile_entities: Query<Entity, (With<Volatile>, Changed<GridCoords>)>,
+    mut all_volatiles: Query<(Entity, &GridCoords, &mut Volatile), Changed<GridCoords>>,
+) {
+    let (mut moved_volatiles, mut stationary_volatiles): (Vec<_>, Vec<_>) = all_volatiles
+        .iter_mut()
+        .partition(|(entity, ..)| moved_volatile_entities.contains(*entity));
+
+    for index in 1..moved_volatiles.len() {
+        let (head_moved_volatiles, remaining_moved_volatiles) = moved_volatiles.split_at_mut(index);
+
+        let (_, grid_coords_a, volatile_a) = head_moved_volatiles.last_mut().expect("TODO");
+
+        if volatile_a.is_solid() {
+            for (_, grid_coords_b, volatile_b) in remaining_moved_volatiles.iter_mut() {
+                if volatile_b.is_solid() && grid_coords_a == grid_coords_b {
+                    volatile_a.sublimate();
+                    volatile_b.sublimate();
+                }
+            }
+        }
+    }
+
+    for (_, grid_coords_a, volatile_a) in moved_volatiles.iter_mut() {
+        if volatile_a.is_solid() {
+            for (_, grid_coords_b, volatile_b) in stationary_volatiles.iter_mut() {
+                if volatile_b.is_solid() && grid_coords_a == grid_coords_b {
+                    volatile_a.sublimate();
+                    volatile_b.sublimate();
+                }
+            }
         }
     }
 }
