e7bc2cfcc1469c99e2bc12d64592422efbf55c17
Trevor Lovell <trevorlovelldesign@gmail.com>
2024-11-21 21:58:58 -0700

    fix: make gravestone movement queries sensitive to sublimation

--- src/graveyard/gravestone_movement_queries.rs
+++ src/graveyard/gravestone_movement_queries.rs
@@ -3,9 +3,11 @@ use bevy_ecs_ldtk::prelude::*;
 
 use crate::graveyard::{arrow_block::MovementTile, gravestone::GraveId};
 
+use super::volatile::Volatile;
+
 #[derive(Debug, SystemParam)]
 pub struct GravestoneMovementQueries<'w, 's> {
-    gravestones: Query<'w, 's, (&'static GridCoords, &'static GraveId)>,
+    gravestones: Query<'w, 's, (&'static GridCoords, &'static GraveId, &'static Volatile)>,
     movement_tiles: Query<'w, 's, (&'static GridCoords, &'static MovementTile)>,
 }
 
@@ -13,8 +15,10 @@ impl<'w, 's> GravestoneMovementQueries<'w, 's> {
     pub fn find_movement(&self, grave_id: &GraveId) -> Option<&MovementTile> {
         self.gravestones
             .iter()
-            .find(|(_, this_grave_id)| &grave_id == this_grave_id)
-            .and_then(|(grid_coords, _)| {
+            .find(|(_, this_grave_id, volatile)| {
+                **volatile == Volatile::Solid && &grave_id == this_grave_id
+            })
+            .and_then(|(grid_coords, ..)| {
                 self.movement_tiles
                     .iter()
                     .find(|(this_grid_coords, _)| &grid_coords == this_grid_coords)
