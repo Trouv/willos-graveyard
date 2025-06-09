947a16a65daefdc09374914747c61daf78d1d856
Trevor Lovell <trevorlovelldesign@gmail.com>
2025-06-08 20:31:57 -0600

    docs: document layer module

--- src/graveyard/layer.rs
+++ src/graveyard/layer.rs
@@ -1,18 +1,28 @@
+//! Utilities related to the graveyard's LDtk layers.
+
 use bevy::prelude::*;
 use bevy_ecs_ldtk::prelude::*;
 use thiserror::Error;
 
+/// Layer entity does not exist.
 #[derive(Debug, Error)]
 #[error("{0:?} layer entity does not exist.")]
 pub struct LayerEntityDoesNotExist(GraveyardLayer);
 
+/// All LDtk layers in graveyard levels.
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum GraveyardLayer {
+    /// The background layer, usually for ground textures.
     Background,
+    /// Another background layer, specifically for border textures that should appear behind willo.
     BorderBackground,
+    /// Another background layer, but for entities that should appear behind willo.
     BackgroundEntities,
+    /// Layer for gameplay tiles, like walls and volatile textures.
     IntGrid,
+    /// Layer for sokoban entities, like willo and pushable blocks.
     Entities,
+    /// Layer for things that appear infront of willo, like certain parts of border textures.
     Foreground,
 }
 
@@ -35,6 +45,7 @@ impl GraveyardLayer {
         }
     }
 
+    /// Returns a system that takes a pipe input of bundles and spawns them on this layer.
     pub fn spawn_bundles_on<I>(
         self,
     ) -> impl Fn(In<I>, Commands, Query<(Entity, &LayerMetadata)>) -> Result<(), LayerEntityDoesNotExist>
