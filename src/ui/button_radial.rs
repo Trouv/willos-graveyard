77a5798da86c8428c5f46e95dc87501a2df16d0c
Trevor Lovell <trevorlovelldesign@gmail.com>
2025-08-23 18:38:09 -0600

    fix: set image color instead of background color

--- src/ui/button_radial.rs
+++ src/ui/button_radial.rs
@@ -24,22 +24,22 @@ pub struct ButtonRadial;
 /// System that alters the visuals of a button radial to show interaction.
 fn highlight_button_radial(
     text_buttons: Query<(Entity, &Interaction), Changed<Interaction>>,
-    mut button_radials: Query<(&mut BackgroundColor, &ChildOf), With<ButtonRadial>>,
+    mut button_radials: Query<(&mut ImageNode, &ChildOf), With<ButtonRadial>>,
 ) {
     for (button_entity, interaction) in text_buttons.iter() {
-        if let Some((mut radial_color, _)) = button_radials
+        if let Some((mut image_node, _)) = button_radials
             .iter_mut()
             .find(|(_, child_of)| child_of.parent() == button_entity)
         {
             match interaction {
                 Interaction::None => {
-                    *radial_color = BackgroundColor(Color::NONE);
+                    image_node.color = Color::NONE;
                 }
                 Interaction::Hovered => {
-                    *radial_color = BackgroundColor(Color::WHITE);
+                    image_node.color = Color::WHITE;
                 }
                 Interaction::Pressed => {
-                    *radial_color = BackgroundColor(GRAY.into());
+                    image_node.color = GRAY.into();
                 }
             }
         }
